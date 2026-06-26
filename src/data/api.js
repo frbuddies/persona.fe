f95e9f3d03c8ef5f1a44e410dde98971c54d05f4
fb <freelacingbuddies@gmail.com>
2026-06-26 21:33:26 +0530

    A

--- src/data/api.js
+++ src/data/api.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = 'http://personabe-production.up.railway.app/persona';
+const BACKEND_URL = 'https://personabe-production.up.railway.app/persona';
 
 export async function sendResultsToBackend(data) {
   try {
